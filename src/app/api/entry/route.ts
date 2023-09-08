63eaad412d226c4655b741d80fcdd3410a7cbfb4
sceiler <sceiler@users.noreply.github.com>
2023-09-08 11:37:23 +0200

    make more fancy

--- src/app/api/entry/route.ts
+++ src/app/api/entry/route.ts
@@ -4,24 +4,37 @@ import { getServerSession } from "next-auth";
 import { NextResponse } from "next/server";
 
 export async function POST(request: Request) {
-    const session = await getServerSession(authOptions);
-    const currentUserEmail = session?.user?.email;
+  const session = await getServerSession(authOptions);
+  const currentUserEmail =
+    (session && session.user && session.user.email) || undefined;
 
-    const data = await request.json();
-    
-    const entry = await prisma.entry.create({
-        data: {
-            title: data.title,
-            content: data.content,
-            userId: 'clm9kypg90000ugcnnz13a6ov'
-        },});
+  const data = await request.json();
+  const user = await prisma.user.findUnique({
+    where: {
+      email: currentUserEmail,
+    },
+  });
 
-    return NextResponse.json(entry);
+  if (!user) {
+    return NextResponse.json({ error: "User not found" });
+  }
+
+  const entry = await prisma.entry.create({
+    data: {
+      title: data.title,
+      content: data.content,
+      userId: user?.id,
+    },
+  });
+
+  return NextResponse.json(entry);
 }
 
 export async function GET(request: Request) {
-    const session = await getServerSession(authOptions);
-    const entries = await prisma.entry.findMany({ where: { user: session?.user}});
+  const session = await getServerSession(authOptions);
+  const entries = await prisma.entry.findMany({
+    where: { user: session?.user },
+  });
 
-    return NextResponse.json(entries);
+  return NextResponse.json(entries);
 }
